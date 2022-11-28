d42b72513f36159bcd8e709d5647888c70e0bd7e
ptejas55 <108654941+ptejas55@users.noreply.github.com>
2022-11-29 01:42:47 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './style.css';
 
 export default function App() {
   const arr = ['tejas', 'kishan', 'ram'];
-  const username = arr.map((name) => name);
+  const username = arr.map((name) => name + ' L.d');
+  let i = 0;
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p> welcome {username} </p>
+      <p> welcome {username[0]} </p>
+      <p> welcome {username[1]} </p>
+      <p> welcome {username[2]} </p>
     </div>
   );
 }
